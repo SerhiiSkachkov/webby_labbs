aa04e4b731b69b47b4d4c18b559265828b2123c2
SerhiiSkachkov <sergeyskachkov01@gmail.com>
2020-08-26 17:42:43 +0300

    Updates

---
+++ precache-manifest.1d676cc3f43b9dbdcb50f1c0bd84094b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb5bab4c75ca9f9fd90041b6f194a903",
+    "url": "/webby_labbs/index.html"
+  },
+  {
+    "revision": "ea88a6b57e2eb72bb1ed",
+    "url": "/webby_labbs/static/css/main.aa33519a.chunk.css"
+  },
+  {
+    "revision": "e90a3e59128053f25b94",
+    "url": "/webby_labbs/static/js/2.e3a0d8d4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/webby_labbs/static/js/2.e3a0d8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea88a6b57e2eb72bb1ed",
+    "url": "/webby_labbs/static/js/main.fbec888a.chunk.js"
+  },
+  {
+    "revision": "c7fd1aa2eaf91ffb6101",
+    "url": "/webby_labbs/static/js/runtime-main.040564a3.js"
+  },
+  {
+    "revision": "72de91db358fdf015511e4c3c8bc607c",
+    "url": "/webby_labbs/static/media/Robotothin.72de91db.woff2"
+  },
+  {
+    "revision": "7faf23e11b3c2da59062fffc117e3a40",
+    "url": "/webby_labbs/static/media/Robotothin.7faf23e1.ttf"
+  },
+  {
+    "revision": "a11d03f7bc9e68d84afdc5eea52ed121",
+    "url": "/webby_labbs/static/media/Robotothin.a11d03f7.woff"
+  }
+]);
